e141f0935a54e6b37f3107fe933d6baf7d1e5185
kanhiya10 <kanhiyagandhi002@gmail.com>
2025-09-16 14:18:06 +0530

    fix: deploy

--- src/app/callBack/page.tsx
+++ src/app/callBack/page.tsx
@@ -17,7 +17,7 @@ const CallbackPage = () => {
 
     const fetchToken = async () => {
       try {
-        const res = await fetch("http://localhost:3000/api/auth/token", {
+        const res = await fetch("api/auth/token", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ code }),
